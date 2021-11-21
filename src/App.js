edbc2bc91824b14ce431671c23478b8944f80ed7
Albert de Haro <Albert.deharo.murcia@gmail.com>
2021-11-21 22:42:15 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Section1 from './../src/components/section1';
-import Section2 from './../src/components/section2';
-import Section3 from './../src/components/section3';
-import Text from './../src/components/textBox';
+import Section1 from './components/section1';
+import Section2 from './components/section2';
+import Section3 from './components/section3';
+import Text from './components/textBox';
+import aviator from './../src/assets/aviator.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Section1/>
+      <div className="section-container">
+        <img src={aviator} alt="" />
+      </div>
+      <div className="spanTest">
+        <span >123</span>
+      </div>
       <Text/>
       <Section2/>
       <Text/>
--- src/components/section1.js
+++ src/components/section1.js
@@ -1,11 +1,21 @@
 import { Parallax } from 'react-parallax';
 import lofoten_norway from './../assets/lofoten_norway.jpg';
 
+// import './nav.scss'
+
 const Section1 = () => (
     <Parallax className="image" bgImage={lofoten_norway} strength={200}>
+        <div className="nav">
+            <ul>
+                <li>link 1</li>
+                <li>link 2</li>
+                <li>link 3</li>
+            </ul>
+        </div>
         <div className="content">
+            <span></span>
             <span className="img-txt">
-            Content goes here. Parallax height grows with content height.
+            WIN A TRIP
             </span>
         </div>
     </Parallax>
